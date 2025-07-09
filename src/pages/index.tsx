759d8c6cb8dd7b8f35e113177cdf39dcb1632840
sumu <sumu@jit.pro>
2025-07-09 08:46:37 +0800

    调整首页卡片样式和图标，优化用户体验
    - 修改卡片的内边距，从2.5rem调整为2.0rem，提升视觉效果。
    - 更新多个卡片的图标，增强内容的直观性和吸引力。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ const CONTENT = {
     subtitle: '企业级应用和 AI 智能体的开发、调测、发布、部署、更新的全栈一体化平台',
     cards: [
       {
-        icon: '📋',
+        icon: '📡',
         title: 'JIT AI应用协议',
         description: 'Jit AI App Protocol，简称 JAAP，灵活开放的面向AI的解释型应用架构协议，智能化动态感知和调用、GUI图形化实时编排和构建。'
       },
@@ -42,17 +42,17 @@ const CONTENT = {
         description: '为 AI 集成和 GUI 编排而生的编排式编程框架，简洁、灵活、高集成度、无限扩展，极简化业务应用层。'
       },
       {
-        icon: '🛠️',
+        icon: '🧩',
         title: 'JIT 编排编程工具',
         description: '编排式定义业务模块、组合模块，编程式开发业务处理逻辑，自动生成高质量代码，极大降低门槛、提供效率和质量。'
       },
       {
-        icon: '🔧',
+        icon: '🛠️',
         title: 'JIT DevOps运维管理工具',
         description: '服务器端也像浏览器端一样的"自动按需加载最新应用"，私有化环境下自动部署更新。'
       },
       {
-        icon: '🔄',
+        icon: '🔗',
         title: 'JIT 应用级继承机制',
         description: '零依赖、无侵入、可改写、可扩展的应用和模块的复用机制，优雅的解决复用和扩展的矛盾。'
       }

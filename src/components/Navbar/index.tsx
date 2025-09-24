8beaea4bdbbc0c0b74fa6c8a818caedf97d5efb0
sumu <wuhuaqiao@dingtalk.com>
2025-09-24 15:19:32 +0800

    fix: 隐藏首页导航下的默认导航栏

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -17,6 +17,12 @@ const Navbar: React.FC<NavbarProps> = ({ currentLocale }) => {
   const CONTENT = currentLocale === 'zh' ? CONTENT_ZH : CONTENT_EN;
 
   useEffect(() => {
+    
+    // 为首页添加特殊类名，用于隐藏Docusaurus默认导航栏
+    setTimeout(() => {
+      document.body.classList.add('home-page');
+    }, 10);
+    
     const checkMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     };

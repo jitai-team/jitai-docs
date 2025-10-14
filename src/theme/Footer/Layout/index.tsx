241536f2bc9cb03f36f0d5e239f9a32b119b61b9
sumu <anonusers@icloud.com>
2025-10-14 14:41:02 +0800

    feat: add toolfame link (#274)
    Co-authored-by: sumu <sumu@jit.pro>

---
+++ src/theme/Footer/Layout/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import type {Props} from '@theme/Footer/Layout';
+
+export default function FooterLayout({
+  style,
+  links,
+  logo,
+  copyright,
+}: Props): React.JSX.Element {
+  return (
+    <footer
+      className={clsx('footer', {
+        'footer--dark': style === 'dark',
+      })}>
+      <div className="container container-fluid">
+        {links}
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && <div className="margin-bottom--sm">{logo}</div>}
+            {/* toolfame 徽章 */}
+            <div style={{ marginBottom: '1rem', display:'none' }} className='external-link'>
+              <a 
+                href="https://toolfame.com/item/jitai" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <img 
+                  src="https://toolfame.com/badge-dark.svg" 
+                  alt="Featured on toolfame.com" 
+                  style={{ height: '54px', width: 'auto', opacity: 0.6 }} 
+                />
+              </a>
+            </div>
+            {copyright}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
+

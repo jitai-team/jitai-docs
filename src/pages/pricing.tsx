e302b46c0b5b63e4325fd5f7a7492fdb95b2e326
shisongqing <layne1993@dingtalk.com>
2025-12-10 11:37:48 +0800

    fix: 官网调整

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import PageLayout from '@site/src/components/PageLayout';
-import PricingSection from '@site/src/components/CompsPricing/PricingSection';
-import PricingFAQSection from '@site/src/components/CompsPricing/PricingFAQSection';
-import styles from '@site/src/pages/pricing.module.css';
+import React from "react";
+import Head from "@docusaurus/Head";
+import PageLayout from "@site/src/components/PageLayout";
+import PricingSection from "@site/src/components/CompsPricing/PricingSection";
+import PricingFAQSection from "@site/src/components/CompsPricing/PricingFAQSection";
+import styles from "@site/src/pages/pricing.module.css";
 
 const PricingPage: React.FC = () => {
-  return (
-    <PageLayout 
-      pageId="pricing"
-      containerClassName={styles.container}
-    >
-      <PricingSection />
-      <PricingFAQSection />
-    </PageLayout>
-  );
+    return (
+        <PageLayout pageId="pricing" containerClassName={styles.container}>
+            <Head>
+                <meta name="robots" content="noindex, nofollow" />
+            </Head>
+            <PricingSection />
+            <PricingFAQSection />
+        </PageLayout>
+    );
 };
 
 export default PricingPage;
